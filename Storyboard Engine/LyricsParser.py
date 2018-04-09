b3871cfa720a5856df15ea9dd06f32f336b8d66a
frankhjwx <frankhjwx@gmail.com>
2018-04-09 21:00:28 +0800

    added test for karaoke ass

--- Storyboard Engine/LyricsParser.py
+++ Storyboard Engine/LyricsParser.py
@@ -151,12 +151,12 @@ if __name__ == '__main__':
     # Then create lyricParser, or directly add sentences
     CR = CharacterRenderer(font_path='Fonts/LEVIBRUSH.TTF', file_path='SB/letters/')
     LP = LyricParser(CR)
-    LP.ass_reader('Subtitles\subtitles.ass')
+    LP.ass_reader('Subtitles\hana ni natta.ass')
     sentences = LP.get_sentences()
-    sentences.append(Sentence('麻花牛逼', 500, 1000, CR))
-    sentences.append(Sentence('yf大师牛逼', '00:50:123', '01:12:123', CR))
+    #sentences.append(Sentence('麻花牛逼', 500, 1000, CR))
+    #sentences.append(Sentence('yf大师牛逼', '00:50:123', '01:12:123', CR))
     for sen in sentences:
         print(sen.content, sen.start_t, sen.end_t)
         for ch in sen.letters:
-            print(ch.character, ch.width, ch.start_t, ch.end_t)
+            print(' ', ch.character, ch.width, ch.start_t, ch.end_t)
     #CR.render()
